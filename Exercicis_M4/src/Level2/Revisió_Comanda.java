b7cb9b55b508a19668b7af62fc12f4934e17d477
dasucard <dasucard@gmail.com>
2021-02-10 17:20:48 +0100

    Ejercicio nivel 2 en progreso

---
+++ Exercicis_M4/src/Level2/Revisió_Comanda.java
@@ -0,0 +1,118 @@
+package Level2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class Revisió_Comanda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				//Declaración Array de los platos.
+				String Menú[] = new String[6];
+						
+				//Declaración Array de los precios de cada plato
+				int Preu[] = new int[6];
+				
+				//HashMap
+				HashMap<String, Integer> diccionari = new HashMap<String, Integer>();
+				
+				//Declaración de la Lista
+				ArrayList<String>Cuenta=new ArrayList<String>(); 
+				
+				for (int i=0; i<6; i++){
+					
+					Scanner entrada  =new Scanner(System.in); //Lee la consola 
+					
+					System.out.println("Introduce plato, por favor");
+					
+					String plato= entrada.nextLine();
+					
+					System.out.println("Introduce precio, por favor");
+					
+					int precio= entrada.nextInt();
+					
+					//entrada.close();
+					
+					Preu[i]= precio; //Inicialización del segundo Array
+					
+					Menú[i]= plato;//Inicialización del primer Array
+					
+					diccionari.put(plato, precio);//Inicialización del HashMap.
+					
+					//System.out.println("Plato de " + plato + " añadido."); //me imprime el plato añadido.
+					
+				}
+
+				//Muestra en consola los platos del menú añadidos por el restaurante.
+				for (int i=0;i<6;i++){
+					
+					System.out.println("plato " + (i+1) + " : " + Menú[i] + " " + Preu[i] +"€");
+					
+				}
+				
+					
+				System.out.println();
+					
+				int	seguir=1;
+				
+				int i=0;
+				
+				System.out.println("Pedido de: ");
+					
+				while (seguir==1){
+					
+					String quiero = JOptionPane.showInputDialog("Escribe el plato que deseas, por favor: ");
+					
+					Cuenta.add(quiero);
+					
+					System.out.println(Cuenta.get(i));
+					
+					i++;
+							
+					seguir= Integer.parseInt(JOptionPane.showInputDialog("¿Quieres seguir pidiendo? 1:Si / 0:No"));
+					
+				}
+				
+				System.out.println();
+				 
+				System.out.println("Recibo: ");
+				
+				try {
+					
+					int total=0;
+					
+					for (int j=0; j<Cuenta.size(); j++){
+										
+							for (String i1 : diccionari.keySet()){
+									
+								if (Cuenta.get(j).equalsIgnoreCase(i1)){
+								   
+								   total = total + diccionari.get(i1);	      
+										
+								}
+
+									
+						}
+						
+					} 
+					
+					System.out.println("El precio a pagar es de: " + total + "€");
+					
+					
+				}catch (Exception e){
+					
+					System.out.println("Algo ha ido mal");
+					
+				}finally{
+					
+					System.out.println("Uno de los platos no esta en el menú");
+					
+				}
+
+	}
+
+}
